aab14f5c8f44008aea6e170118e48a5f76e435b9
MarkLicud <168145592+MarkLicud@users.noreply.github.com>
2024-04-26 13:28:01 +0800

    Add files via upload

---
+++ number-challenge.js
@@ -0,0 +1,28 @@
+/*
+    1 Create a function that will return the sum 3 numbers
+    2 Create a function that will return the difference of the 2 numbers
+    3 Create a function that will return the product of the 2 numbers
+    4 Create a function that will return the average of the 2 numbers
+*/
+
+function sumThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+function differenceOfTwoNumbers(num1, num2) {
+    return num1 - num2;
+}
+
+function productOfTwoNumbers(num1, num2) {
+    return num1 * num2;
+}
+
+function averageOfTwoNumbers(num1, num2) {
+    return (num1 + num2) / 2;
+}
+
+
+console.log(sumThreeNumbers(1, 2, 1)); 
+console.log(differenceOfTwoNumbers(6, 4)); 
+console.log(productOfTwoNumbers(2, 4)); 
+console.log(averageOfTwoNumbers(6, 4)); 
